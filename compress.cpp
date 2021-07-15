d6b1e6f9f63d432d8f729fd11679183007c39522
Luai Anwar <55032660+LoayAnwar@users.noreply.github.com>
2021-07-15 14:43:21 +0200

    compress and decompress edited

--- compress.cpp
+++ compress.cpp
@@ -9,9 +9,9 @@ extern MainWindow *w;
 extern int flag;
 extern QString json_file;
 
-int mul_char_code = 256;
+
 vector<int> code;
-map<string, int> encoded_table;
+
 
 Compress::Compress(QWidget *parent) :
     QWidget(parent),
@@ -26,23 +26,15 @@ Compress::Compress(QWidget *parent) :
     QIcon icon(pixMap);
     QPixmap pixMap_2(":\\rec\\icons\\decompress.png");
     QIcon icon_2(pixMap_2);
-    QPixmap pixMap_3(":\\rec\\icons\\compress2.png");
-    QIcon icon_3(pixMap_3);
 
     pushButton->setText("Save File");
     pushButton->setIcon(icon);
     pushButton_2->setText("Decompress File");
     pushButton_2->setIcon(icon_2);
-    pushButton_3->setText("Compress File");
-    pushButton_3->setIcon(icon_3);
-
-    pushButton_3->hide();
-
-    textEdit->setReadOnly(true);
     textEdit->setFontPointSize(13);
+
     horizontal_buttons_layout->addWidget(pushButton);
     horizontal_buttons_layout->addWidget(pushButton_2);
-    horizontal_buttons_layout->addWidget(pushButton_3);
     vertical_tab_layout->addLayout(horizontal_buttons_layout);
     vertical_tab_layout->addWidget(textEdit);
     main_tab_layout->addLayout(vertical_tab_layout);
@@ -57,9 +49,11 @@ Compress::Compress(QWidget *parent) :
         s1 = json_file.toStdString();
     }
     QString text_encoded;
-    code = compress_file(s1, mul_char_code);
+    code.clear();
+    code = compress_file(s1);
     for (int i = 0; i < code.size(); i++) {
         text_encoded += QString::number(code[i]);
+        text_encoded += " ";
     }
     textEdit->setText(text_encoded);
 }
@@ -67,7 +61,6 @@ Compress::Compress(QWidget *parent) :
 void Compress::connect_tab_fn() {
     connect(pushButton, SIGNAL(clicked()), this, SLOT(on_pushButton_clicked()));
     connect(pushButton_2, SIGNAL(clicked()), this, SLOT(on_pushButton_2_clicked()));
-    connect(pushButton_3, SIGNAL(clicked()), this, SLOT(on_pushButton_3_clicked()));
 }
 
 void Compress::on_pushButton_clicked() {
@@ -75,41 +68,23 @@ void Compress::on_pushButton_clicked() {
 }
 
 void Compress::on_pushButton_2_clicked() {
-    pushButton_2->hide();
-    pushButton_3->show();
-    map<int, string> table_decode;
-    map<string, int>::iterator itr;
-    for (itr = encoded_table.begin(); itr != encoded_table.end(); itr++) {
-        table_decode[itr->second] = itr->first;
-    }
-    decode(table_decode, code);
+//    map<int, string> table_decode;
+//    map<string, int>::iterator itr;
+//    for (itr = encoded_table.begin(); itr != encoded_table.end(); itr++) {
+//        table_decode[itr->second] = itr->first;
+//    }
+    decode(code);
+    code.clear();
 }
 
-void Compress::on_pushButton_3_clicked() {
-    pushButton_3->hide();
-    pushButton_2->show();
-    vector<int> code;
-    QString text_encoded;
-    string s1;
-    if (flag == 1) {
-        XML_Tree->minify(s1);
-    }
-    else if (flag == 2) {
-        s1 = json_file.toStdString();
-    }
-    code = compress_file(s1, mul_char_code);
-    for (int i = 0; i < code.size(); i++) {
-        text_encoded += QString::number(code[i]);
-    }
-    textEdit->setText(text_encoded);
-}
-
-vector<int> Compress::compress_file(string file_encode, int &mul_char_code) {
+vector<int> Compress::compress_file(string file_encode) {
+    map<string, int> encoded_table;
     for (int i = 0; i <= 255; i++) {
         string character = "";
         character += char(i);
         encoded_table[character] = i;
     }
+    int mul_char_code = 256;
     vector<int> encoded_code;
     string current = "", next = "";
     current += file_encode[0];
@@ -132,15 +107,44 @@ vector<int> Compress::compress_file(string file_encode, int &mul_char_code) {
     return encoded_code;
 }
 
-void Compress::decode(map<int, string> table_decode, vector<int> encoded_code) {
+void Compress::decode(vector<int> encoded_code) {
     string s;
-    for (int i = 0; i < encoded_code.size(); i++) {
-        s += table_decode[encoded_code[i]];
+//    for (int i = 0; i < encoded_code.size(); i++) {
+//        s += table_decode[encoded_code[i]];
+//    }
+    map<int, string> decode_table;
+    for (int i = 0; i <= 255; i++) {
+        string character = "";
+        character += char(i);
+        decode_table[i] = character;
+    }
+    int pre = encoded_code[0];
+    int next;
+    string str = decode_table[pre];
+    string current = "";
+    current += str[0];
+    s += str;
+    //textEdit->append(QString::fromStdString(str));
+    int mul_char_count = 256;
+    for (int i = 0; i < encoded_code.size() - 1; i++) {
+        next = encoded_code[i + 1];
+        if (decode_table.find(next) == decode_table.end()) {
+            str = decode_table[pre];
+            str = str + current;
+        }
+        else {
+            str = decode_table[next];
+        }
+        s += str;
+        //textEdit->append(QString::fromStdString(str));
+        current = "";
+        current += str[0];
+        decode_table[mul_char_count] = decode_table[pre] + current;
+        mul_char_count++;
+        pre = next;
     }
     QString text_decoded = QString::fromStdString(s);
     textEdit->setText(text_decoded);
-    encoded_table.clear();
-    mul_char_code = 256;
 }
 
 Compress::~Compress()
